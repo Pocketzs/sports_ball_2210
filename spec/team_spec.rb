f286a6606c3448a440e2800c8e6b4f45aca6d545
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-23 21:06:45 -0500

    test: Add team add_player test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -35,4 +35,17 @@ RSpec.describe Team do
       expect(team.player_count).to eq(0)
     end
   end
+
+  describe '#add_player' do
+    it 'can add a player to the team roster' do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous", 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+
+      expect(team.roster).to eq([player_1, player_2])
+    end
+  end
 end
