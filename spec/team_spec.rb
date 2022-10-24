c040b0b771b1202903433ecfa9584bb70fdcdefc
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-23 20:57:50 -0500

    test: Add team name test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe Team do
 
       expect(team).to be_a(Team)
     end
+
+    it 'has a team name' do
+      team = Team.new("Dodgers", "Los Angeles")
+
+      expect(team.name).to eq("Dodgers")
+    end
   end
 end
